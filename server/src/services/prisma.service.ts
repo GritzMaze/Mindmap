2e18da75b01c0765d7e6cfd3056fa2bc657c3d36
GritzMaze <jivkovvesselin@gmail.com>
2023-04-15 17:14:52 +0300

    Create prisma service as a single instance so we don't bottleneck the database

---
+++ server/src/services/prisma.service.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from '@prisma/client';
+import { config } from '../config/config';
+
+class PrismaService extends PrismaClient {
+
+    readonly readClient: PrismaClient;
+
+  constructor() {
+    super({
+        datasources: {
+            db: {
+                url: config.get('db.databaseUrl'),
+            },
+        }
+        // Uncomment the following line to enable debug mode
+        // Will log all queries to the console
+        // log: ['query', 'info', 'warn', 'error'],
+    });
+
+    this.readClient = new PrismaClient({
+        datasources: {
+            db: {
+                url: config.get('db.databaseUrl'),
+            },
+        },
+        // log: ['query', 'info', 'warn', 'error'],
+    });
+  }
+
+  async init(): Promise<void> {
+    await this.$connect();
+    await this.readClient.$connect();
+  }
+
+  async exit(): Promise<void> {
+    await this.$disconnect();
+    await this.readClient.$disconnect();
+  }
+}
+
+export const prismaService = new PrismaService();
