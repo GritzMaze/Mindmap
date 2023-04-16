1135e6c4f917453bda84948b057e3c52f2b15971
GritzMaze <jivkovvesselin@gmail.com>
2023-04-17 00:16:33 +0300

    Extend login and register to use bcrypt to encrypt passwords

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,11 +1,25 @@
 import { User } from '@prisma/client';
 import { jwtService } from './jwt.service';
 import { UserCreateInput, userService } from '.';
+import bcrypt from 'bcrypt';
+import { config } from '../config/config';
 
 class AuthService {
     async login(user: User): Promise<string> {
+        const serverUser = await userService.find(user.id);
+
+        if (!serverUser) {
+            return null;
+        }
+
+        const passwordMatch = await bcrypt.compare(user.password, serverUser.password);
+
+        if (!passwordMatch) {
+            return null;
+        }
+
         const payload = { username: user.username, id: user.id };
-        
+
         return jwtService.sign(payload);
     }
 
@@ -13,7 +27,16 @@ class AuthService {
         return await userService.find(payload.id);
     }
 
-    async register(user: UserCreateInput): Promise<User> {
+    async register(user: UserCreateInput): Promise<Partial<User>> {
+        const existingUser = await userService.findByUsername(user.username);
+
+        if (existingUser) {
+            throw new Error('User already exists');
+        }
+        const saltRounds = config.get('bcrypt.saltRounds');
+
+        user.password = await bcrypt.hash(user.password, saltRounds);
+
         return await userService.create(user);
     }
 }
