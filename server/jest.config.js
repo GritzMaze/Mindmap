8259652860ac0ab92d89d8f3a21f10cdbcf8b111
GritzMaze <jivkovvesselin@gmail.com>
2023-04-23 00:26:41 +0300

    Adjust configuration files to work for jest tests

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,5 +1,8 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// eslint-disable-next-line no-undef
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  setupFilesAfterEnv: ['<rootDir>/test/setup.ts']
 };
