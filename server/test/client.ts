94108b8e5873407ce52976ea3c404ed395e8a4b5
GritzMaze <jivkovvesselin@gmail.com>
2023-04-27 23:09:21 +0300

    Add a setup after env files for tests

--- server/test/client.ts
+++ server/test/client.ts
@@ -1,4 +1,11 @@
-import { PrismaService } from '../src/services';
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaService();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+  // log: ['query', 'info', 'warn', 'error'],
+});
 export default prisma;
--- server/test/setup.ts
+++ server/test/setup.ts
@@ -1,25 +1,4 @@
-// import { Prisma } from '@prisma/client';
-// import 'jest';
-// import prisma from './client';
-
 import winston from 'winston';
-
-// const tables = Prisma.dmmf.datamodel.models
-//   .map((model) => model.dbName)
-//   .filter((table) => table);
-
-// global.beforeAll(async () => {
-//   await prisma.$connect();
-//   await prisma.$transaction([
-//     ...tables.map((table) =>
-//       prisma.$executeRawUnsafe(table ? `TRUNCATE ${table} CASCADE;` : ''),
-//     ),
-//   ]);
-// });
-
-// global.afterAll(async () => {
-//   await prisma.$disconnect();
-// });
 
 winston.add(
   new winston.transports.Console({
